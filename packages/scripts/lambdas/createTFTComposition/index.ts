fb4b540a8de444db41178df1da113142037bb6d0
Tony Tang <tony.tang.developer@gmail.com>
2024-09-17 21:53:32 +0200

    feat: ask AI to create comp

--- packages/scripts/lambdas/createTFTComposition/index.ts
+++ packages/scripts/lambdas/createTFTComposition/index.ts
@@ -1,9 +1,57 @@
 import { Handler } from "aws-lambda";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+import OpenAI from "openai";
 
-export const handler: Handler = async (event) => {
-  console.log("Event: ", event);
+const { REGION, OPENAI_SECRET_NAME } = process.env;
+
+const secretsClient = new SecretsManagerClient({ region: REGION });
+let openai: OpenAI;
+let assistantId: string;
+
+export const handler: Handler = async () => {
+  await initOpenAI();
+
+  const { runId, threadId } = await askAIForANewTFTComposition();
   return {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
+    threadId,
+    runId,
   };
 };
+
+async function initOpenAI() {
+  if (!openai) {
+    const secret = await secretsClient.send(
+      new GetSecretValueCommand({ SecretId: OPENAI_SECRET_NAME }),
+    );
+
+    const { openai_api_key: apiKey, openai_assistant_id: openAIAssistantId } =
+      JSON.parse(secret.SecretString ?? "{}");
+
+    if (!apiKey) {
+      throw new Error("No secret found");
+    }
+    openai = new OpenAI({
+      apiKey: apiKey,
+    });
+    assistantId = openAIAssistantId;
+  }
+}
+
+async function askAIForANewTFTComposition() {
+  const thread = await openai.beta.threads.create();
+  await openai.beta.threads.messages.create(thread.id, {
+    role: "user",
+    content: "Create me a TFT composition",
+  });
+
+  const run = await openai.beta.threads.runs.create(thread.id, {
+    assistant_id: assistantId,
+  });
+  return {
+    threadId: thread.id,
+    runId: run.id,
+  };
+}
