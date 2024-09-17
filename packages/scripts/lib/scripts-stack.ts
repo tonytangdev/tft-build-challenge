cc50130f75938f756d2fc39f724600f1136a9b81
Tony Tang <tony.tang.developer@gmail.com>
2024-09-17 21:53:15 +0200

    feat: grant permission to read secrets

--- packages/scripts/lib/scripts-stack.ts
+++ packages/scripts/lib/scripts-stack.ts
@@ -4,6 +4,7 @@ import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as path from "path";
 
 const OPENAI_SECRET_NAME = "OpenAI-tft-build-challenges";
+const REGION = process.env.CDK_DEFAULT_REGION ?? "";
 
 export class ScriptsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,6 +20,13 @@ export class ScriptsStack extends cdk.Stack {
       value: tftCompositionTable.tableName,
     });
 
+    // secret - OpenAI API key
+    const openaiSecret = cdk.aws_secretsmanager.Secret.fromSecretNameV2(
+      this,
+      "OpenAI Secret",
+      OPENAI_SECRET_NAME,
+    );
+
     // lambda - call AI API to build tft composition
     const tftCompositionLambda = new NodejsFunction(
       this,
@@ -28,13 +36,17 @@ export class ScriptsStack extends cdk.Stack {
         handler: "handler",
         entry: path.join(__dirname, "../lambdas/createTFTComposition/index.ts"),
         // pass the secret to the lambda
-        environment: { OPENAI_SECRET_NAME },
+        environment: { OPENAI_SECRET_NAME, REGION },
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 512,
       },
     );
     // add to stack output
     new cdk.CfnOutput(this, "TFT Composition Lambda output", {
       value: tftCompositionLambda.functionArn,
     });
+    // authorize to read the secret
+    openaiSecret.grantRead(tftCompositionLambda);
 
     // lambda - call AI API to know when the output is ready
     const tftCompositionStatusLambda = new NodejsFunction(
@@ -45,13 +57,17 @@ export class ScriptsStack extends cdk.Stack {
         handler: "handler",
         entry: path.join(__dirname, "../lambdas/getTFTComposition/index.ts"),
         // pass the secret to the lambda
-        environment: { OPENAI_SECRET_NAME },
+        environment: { OPENAI_SECRET_NAME, REGION },
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 512,
       },
     );
     // add to stack output
     new cdk.CfnOutput(this, "Get TFT Composition output", {
       value: tftCompositionStatusLambda.functionArn,
     });
+    // authorize to read the secret
+    openaiSecret.grantRead(tftCompositionStatusLambda);
 
     // lambda - save the output in dynamodb
     const tftCompositionSaveLambda = new NodejsFunction(
@@ -63,7 +79,10 @@ export class ScriptsStack extends cdk.Stack {
         entry: path.join(__dirname, "../lambdas/saveTFTComposition/index.ts"),
         environment: {
           TFT_COMPOSITION_TABLE_NAME: tftCompositionTable.tableName,
+          REGION,
         },
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 512,
       },
     );
 
